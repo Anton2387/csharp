b6aeed4a1c2c91511cc5f66aa722ab20a31e0711
Anton23 <antonavdonin87@gmail.com>
2022-10-16 22:26:32 +0300

    example001

---
+++ seminar1/example001/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int square = numberA / (numberB * numberB);
+
+if (square == 1)
+{
+    Console.WriteLine("Первое число, является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число, не является квадратом второго числа");
+}
