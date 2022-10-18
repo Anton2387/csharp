579bba183379df03b025bdba2d881e30c5404e3e
Anton23 <antonavdonin87@gmail.com>
2022-10-18 17:24:35 +0300

    max

---
+++ seminar1dz/example002/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите два числа");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if( numberA == numberB)
+{
+    Console.WriteLine("числа равны");
+}
+
+if( numberA < numberB )
+{
+    Console.Write("max = ");
+    Console.Write(numberB);
+}
+else
+{
+    Console.Write("max = ");
+    Console.Write(numberA);
+}
