1466705ace6abd23bcab355036a7fcef6bd14fa8
Anton23 <antonavdonin87@gmail.com>
2022-10-18 18:06:43 +0300

    все четные числа от 1 до N

---
+++ seminar1dz/example008/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int N = 2;
+
+while(N <= number)
+{
+    Console.Write(N);
+    Console.Write(",");
+    N = N + 2;
+}
