d77d14ae02c1077471791961fcef78c4af773708
Anton23 <antonavdonin87@gmail.com>
2022-10-18 16:53:23 +0300

    example005 последовательность

---
+++ seminar1/example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int N = -number;
+
+while(N <= number)
+{
+    Console.Write(N);
+    Console.Write(",");
+    N = N + 1;
+}
