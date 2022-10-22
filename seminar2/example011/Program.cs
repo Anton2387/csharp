76643899dc4e67771429071a7352e4e84f2768e7
Anton23 <antonavdonin87@gmail.com>
2022-10-22 21:02:30 +0300

    выводит случайно трехзначное число и удаляет вторую цифру этого числа

---
+++ seminar2/example011/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = GetRandomNumber(100, 1000);
+int result = DeleteSecondNumber(number);
+Console.WriteLine(number);
+Console.WriteLine(result);
+
+int DeleteSecondNumber(int number)
+{
+    int lastValue = number % 10;
+    number = number / 100;
+    int result = number * 10 + lastValue;
+    return result;
+}
+
+int GetRandomNumber(int minValue, int maxValue)
+{
+    Random random = new Random();
+    int randomNumber = random.Next(minValue, maxValue);
+    return randomNumber;
+}
