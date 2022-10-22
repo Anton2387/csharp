80aef0af09412973a4ee5619a90688182d06977d
Anton23 <antonavdonin87@gmail.com>
2022-10-22 21:57:33 +0300

    проверка на кратность с помощью метода

---
+++ seminar2/example012/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите два любых числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int dif = Check(number1, number2);
+
+if(dif == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно, остаток {dif}");
+}
+
+int Check(int number1, int number2)
+{
+    int dif = number1 % number2;
+    return dif;
+}
