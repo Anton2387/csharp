aedc3e704619fb79889c960d1ea4ae7f1045aa3a
Anton23 <antonavdonin87@gmail.com>
2022-10-22 22:14:31 +0300

    проверка на кратность одновременно для двух чисел

---
+++ seminar2/example014/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int dif = Check(number);
+
+int Check(int number)
+{
+    int dif = number % 7;
+    int dif2 = number % 23;
+    if(dif == 0& dif2 == 0)
+    {
+        Console.WriteLine("Кратно двум числам 7 и 23");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно двум числам 7 и 23");
+    }
+    return dif;
+}
