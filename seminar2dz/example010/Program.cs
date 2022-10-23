5901fc0f7ac4c901f07860c82fda469e5a75e404
Anton23 <antonavdonin87@gmail.com>
2022-10-23 12:49:42 +0300

    определение второй цифры трехзначного числа

---
+++ seminar2dz/example010/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456-> 5
+//782-> 8
+//918-> 1
+//22->Ошибка.
+//1234->Ошибка.
+// Выполнять задание не используя string.
+
+Console.WriteLine("Введите любое трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = Search(number);
+
+int Search(int number)
+{
+    int last = number % 100;
+    int result = last / 10;
+    if(number > 99& number < 1000)
+    {
+        Console.WriteLine($"Вторая цифра введенного числа {result}");
+    }
+    else
+    {
+        Console.WriteLine("Ошибка");
+    }
+    return result;
+}
