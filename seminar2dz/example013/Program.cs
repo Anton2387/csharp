cc478c25e7bf12e56d93f476dc75b7192b8f74a4
Anton23 <antonavdonin87@gmail.com>
2022-10-24 19:55:59 +0300

    выводим третью цирфу заданного числа

---
+++ seminar2dz/example013/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного 
+//числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+// выполнять задание не используя string.
+
+Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+Search(number);
+
+void Search(int ert)
+{
+    if (ert > 99)
+    {
+        int count = -3;
+        int last = ert;
+
+        while (last > 0)
+        {
+            last = last / 10;
+            count = count + 1;
+        }
+
+        int magic = 1;
+
+        while(count > 0)
+        {
+            magic = magic * 10;
+            count = count - 1;
+        }
+
+        ert = ert / magic % 10;
+        
+        Console.WriteLine($"Третья цифра заданного числа: {ert}");
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
