378831ca3c2c154723e79e264cc19e4025aea498
Anton23 <antonavdonin87@gmail.com>
2022-11-02 20:53:14 +0300

    таблица квадратов числа

---
+++ seminar3/example022/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 22: Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25. 2 -> 1,4*/
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int [] SquareNumber(int x)
+{
+    int [] array = new int [x];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = (i + 1) * (i + 1);
+    }
+    return array;
+}
+
+int [] result = SquareNumber(x);
+PrintArray(result);
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array [i]} ");
+    }
+}
