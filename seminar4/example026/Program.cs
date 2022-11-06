0f1b805a4eeb8a5fd7a97c3379a35bf49605588a
Anton23 <antonavdonin87@gmail.com>
2022-11-06 19:44:07 +0300

    количество цифр в числе

---
+++ seminar4/example026/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 26: Напишите программу, которая принимает на вход число 
+и выдаёт количество цифр в числе.
+456 -> 3 78 -> 2 89126 -> 5 */
+
+Console.WriteLine("Введите любое число");
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int a);
+
+if(!isParsed)
+{
+    Console.WriteLine("Не правильно введены данные");
+    return;
+}
+if(a < 1)
+{
+    Console.WriteLine("0");
+    return;
+}
+
+int number = DetermineNumber(a);
+Console.WriteLine(number);
+
+int DetermineNumber(int a)
+{
+    int count = 1;
+
+    for (int i = 9; i < a; i++)
+    {
+        a = a / 10;
+        count += 1;
+    }
+
+    return count;
+}
