691fa1eca6f414a249946d7eabfd7099799e1fc4
Anton23 <antonavdonin87@gmail.com>
2022-11-16 21:38:58 +0300

    определяем количество элментов массива, лежащих в определенном интервале

---
+++ seminar5/example035/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задача 35: Задайте одномерный массив из 123 случайных чисел.
+Найдите количество элементов массива, значения которых лежат в
+отрезке [10,99].
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5*/
+
+
+int[] array = new int[123];
+FillArray(array);
+PrintArray(array);
+int sum = GetCountNumberInRange(array);
+Console.WriteLine($"Количество элементов массива, которые лежат в отрезке от 10 до 99 = {sum}");
+
+int GetCountNumberInRange(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 & array[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine();
+    for (var i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-100, 100);
+    }
+}
