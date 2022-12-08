bdb1d0c38bd0a96954cd1e352c817829a20554d2
Anton23 <antonavdonin87@gmail.com>
2022-12-08 17:04:34 +0300

    программа для копирования заданного массива

---
+++ seminar6/example045/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 45: Напишите программу, 
+которая будет создавать копию заданного массива 
+с помощью поэлементного копирования. */
+
+
+int[] array = GenerateArray(); // возвращаем метод с рандомным массивом
+PrintArray(array);  // печатаем ответ
+
+Console.WriteLine();  // пробел
+
+int[] copyArray = CopyArray(array); // возвращаем метод, со скопированным рандомным массивом
+PrintArray(copyArray);  // печатаем ответ
+
+
+int[] GenerateArray() // создаем метод, для рандомного создания массива
+{
+    int[] array = new int[8];
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 10);
+    }
+    return array;
+}
+
+
+int[] CopyArray(int[] array)  // создаем метод, для копирования рандомного массива
+{
+    int[] сopyArray = new int[array.Length];
+    for (var i = 0; i < array.Length; i++)
+    {
+        сopyArray[i] = array[i];
+    }
+    return сopyArray;
+}
+
+
+void PrintArray(int[] array) // создаем метод, для печати массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
