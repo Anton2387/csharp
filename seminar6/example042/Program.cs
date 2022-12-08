17899b01545225748874e2b667138ab2167dbe8c
Anton23 <antonavdonin87@gmail.com>
2022-12-08 16:15:19 +0300

    преобразование десятичного числа в двоичное

---
+++ seminar6/example042/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 42: Напишите программу, которая будет 
+преобразовывать десятичное число в двоичное. 
+45 -> 101101
+3 -> 11
+2 -> 10*/
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Conversation(number));
+
+int Conversation(int number)
+{
+    int binaryNumber = 0;
+    while (number > 0)
+    {
+        binaryNumber = binaryNumber + number % 2;
+        number = number / 2;
+        binaryNumber = binaryNumber * 10;
+    }
+    binaryNumber = binaryNumber / 10;
+    return binaryNumber;
+}
